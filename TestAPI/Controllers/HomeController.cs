62697aa0867f1d8a7fa3515afb0aaff98353bbea
Pankaj Sah <SAHPAN@chrobinson.com>
2019-08-20 06:00:49 -0500

    first commit

---
+++ TestAPI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            return "Pankaj";
+        }
+        [HttpGet]
+        public string GetName(string name)
+        {
+            return name;
+        }
+    }
+}
